905dc8e942c581dccc650e3884040837bbc69d3c
Nicolás Leiva Büchi <nicolasleivab@gmail.com>
2019-11-15 19:49:38 +0100

    methods as props, two way bindings

--- src/App.js
+++ src/App.js
@@ -12,26 +12,44 @@ class App extends Component {
     secondProp: 'some value'
   };
 
-  changeNameHandler = ()=>{
+  changeNameHandler = (newName)=>{
     //DON'T do this: this.state.persons[0].name = "Frojack";
     this.setState({ //instead use this method included in the Component
       persons: [  //selected prop must contain same amount of elements
-        {name:"Frojack", age:33},
-        {name:"Sminem", age:17},
-        {name:"Bogdanoff", age:77}
+        {name: newName, age:33},
+        {name: "Sminem", age:17},
+        {name: "Bogdanoff", age:77}
       ]
     })
-    console.log(this.state); //props you don't target won't change
+    console.log(this.state); //props you don't target won't change (they merge)  
+                            //using hooks is different, the old state is replaced by the new one
   }
+  newNameHandler = (event)=>{
+    this.setState({ 
+      persons: [ 
+        {name: "Wojak", age:33},
+        {name: event.target.value, age:17}, //target.value for the input value
+        {name: "Bogdanoff", age:77}
+      ]
+  })                        
+}
   //changing state and props are the few things that make react to update the DOM!
   render() {
     return(
     <div className="App">
       <h1>Single Page React App</h1>
-      <button onClick={this.changeNameHandler}>Change name</button>
-      <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>I hate Bitcoin.</Person>
-      <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>I like Bitcoin.</Person>
-      <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>I like Bitcoin.</Person>
+      <button onClick={this.changeNameHandler.bind(this, "Frojack")}>Change name</button>
+      <Person 
+      name={this.state.persons[0].name} 
+      age={this.state.persons[0].age}
+      click={this.changeNameHandler.bind(this, "Jack")}>I hate Bitcoin.</Person> {/*passing methods as props*/}
+      <Person 
+      name={this.state.persons[1].name} 
+      age={this.state.persons[1].age}
+      inputName={this.newNameHandler}>I like Bitcoin.</Person>
+      <Person 
+      name={this.state.persons[2].name} 
+      age={this.state.persons[2].age}>I like Bitcoin.</Person>
     </div>
 )}
 };
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const person = (props) => {
     return <div>
-    <p>I'm {props.name} and I'm {props.age} years old.</p>
+    <p onClick={props.click}>I'm {props.name} and I'm {props.age} years old.</p>
     <p>{props.children}</p>
+    <input type="text" onChange={props.inputName} value={props.name}/>{/*Two way binding to display default names in input*/}
     </div>
 }
 

4fb12736a372420dee2fc9cae4c120f80762117d
Nicolás Leiva Büchi <nicolasleivab@gmail.com>
2019-11-20 06:38:01 +0100

    grid containers

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const person = (props) => {
-    return <div className="Person">
+    return <div className="wrapper Person2">
     {!props.editInputs ?
     <div>
     <button className="btn btn--rect btn--dark-gray" onClick={props.showInputs}>edit</button>
@@ -9,21 +9,22 @@ const person = (props) => {
     <div>
     <button className="btn btn--rect btn--green" onClick={props.showInputs}>save</button>
     </div>}
-    <div className="container">
-    <div className="text text-item--1">{props.name}</div>
-    <div className="text text-item--2">{props.email}</div>
-    <div className="text text-item--3">{props.role}</div>
-    </div>
+    <div className="Person">
     {props.editInputs ?
     <div className="container">
     <input type="text" onChange={props.inputName} value={props.name} maxLength='50'/>
     <input type="text" onChange={props.inputMail} value={props.email} maxLength='50' type="email"/>
     <input type="text" onChange={props.inputRole} value={props.role} maxLength='50'/>
-    </div> : null
+    </div> :    <div className="container">
+    <div className="text text--item">{props.name}</div>
+    <div className="text text--item">{props.email}</div>
+    <div className="text text--item">{props.role}</div>
+    </div>
     }
+    </div>
     <div>
     <button className="btn btn--red" onClick={props.click}>-</button></div>
-    </div> 
+    </div>
     
 }
 

6c6364d76ab123e4733150aa65a9f46a995a0158
Nicolás Leiva Büchi <nicolasleivab@gmail.com>
2020-03-01 19:36:40 -0300

    current team highlight

--- client/src/components/TeamItem/TeamItem.js
+++ client/src/components/TeamItem/TeamItem.js
@@ -5,9 +5,17 @@ import styles from './TeamItem.module.css';
 const TeamItem = ({ team }) => {
   const teamContext = useContext(TeamContext);
 
-  const { setCurrentTeam } = teamContext;
+  const { setCurrentTeam, currentTeam } = teamContext;
 
   const { _id, name } = team;
+
+  const [isActive, setActive] = useState('');
+
+  useEffect(() => {
+    if (currentTeam !== null) {
+      setActive(currentTeam._id);
+    }
+  }, [currentTeam]);
 
   const teamHandler = () => {
     setCurrentTeam(team);
@@ -15,8 +23,12 @@ const TeamItem = ({ team }) => {
 
   return (
     <div className={styles.TeamItem}>
-      <a href='#!' onClick={teamHandler}>
-        {team.name}
+      <a
+        href='#!'
+        onClick={teamHandler}
+        className={_id === isActive ? styles.activeTeam : styles.inactiveTeam}
+      >
+        {name}
       </a>
     </div>
   );

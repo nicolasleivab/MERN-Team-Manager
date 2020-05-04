ae5613fd1b2b13f1350cdf14ec8f7f8027b17280
Nicolás Leiva Büchi <nicolasleivab@gmail.com>
2020-05-04 09:20:06 -0400

    register form update

--- client/src/components/AuthUser/Register.js
+++ client/src/components/AuthUser/Register.js
@@ -1,10 +1,11 @@
-import React, { useState, useContext, useEffect } from 'react';
-import AlertContext from '../../context/alert/alertContext';
-import AuthContext from '../../context/auth/authContext';
-import Alert from './Alert';
-import styles from './Register.module.css';
+import React, { useState, useContext, useEffect } from "react";
+import { Link } from "react-router-dom";
+import AlertContext from "../../context/alert/alertContext";
+import AuthContext from "../../context/auth/authContext";
+import Alert from "./Alert";
+import styles from "./Register.module.css";
 
-const Register = props => {
+const Register = (props) => {
   const alertContext = useContext(AlertContext);
   const authtContext = useContext(AuthContext);
 
@@ -13,87 +14,84 @@ const Register = props => {
 
   useEffect(() => {
     if (isAuthenticated) {
-      props.history.push('/');
+      props.history.push("/");
     }
-    if (error === 'User already exists') {
-      setAlert(error, 'Red');
+    if (error === "User already exists") {
+      setAlert(error, "Red");
       clearErrors();
     }
     // eslint-disable-next-line
   }, [error, isAuthenticated, props.history]);
 
   const [user, setUser] = useState({
-    name: '',
-    email: '',
-    password: '',
-    password2: ''
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
   });
 
   const { name, email, password, password2 } = user;
 
-  const onChange = e => setUser({ ...user, [e.target.name]: e.target.value });
+  const onChange = (e) => setUser({ ...user, [e.target.name]: e.target.value });
 
-  const onSubmit = e => {
+  const onSubmit = (e) => {
     e.preventDefault();
-    if (name === '' || email === '' || password === '' || password2 === '') {
-      setAlert('Missing Fields', 'Red');
+    if (name === "" || email === "" || password === "" || password2 === "") {
+      setAlert("Missing Fields", "Red");
     } else if (password !== password2) {
-      setAlert('Passwords do not match', 'Red');
+      setAlert("Passwords do not match", "Red");
     } else {
       registerUser(user);
     }
   };
 
   return (
-    <div>
+    <div className={styles.Register}>
       <p className={styles.title}>Account Register</p>
       <form className={styles.formContainer} onSubmit={onSubmit}>
         <Alert />
-        <label htmlFor='name'>{name !== '' && 'Name'}</label>
         <input
-          type='text'
-          placeholder='Name'
-          name='name'
+          type="text"
+          placeholder="Name"
+          name="name"
           value={name}
-          required='required'
+          required="required"
           onChange={onChange}
           maxLength={35}
         />
-        <label htmlFor='email'>{email !== '' && 'Email'}</label>
         <input
-          type='email'
-          placeholder='Email'
-          name='email'
+          type="email"
+          placeholder="Email"
+          name="email"
           value={email}
-          required='required'
+          required="required"
           onChange={onChange}
           maxLength={35}
         />
-        <label htmlFor='password'>{password !== '' && 'Password'}</label>
         <input
-          type='password'
-          placeholder='Password'
-          name='password'
+          type="password"
+          placeholder="Password"
+          name="password"
           value={password}
-          required='required'
+          required="required"
           onChange={onChange}
-          minLength='6'
+          minLength="6"
           maxLength={35}
         />
-        <label htmlFor='password2'>
-          {password2 !== '' && 'Confirm Password'}
-        </label>
         <input
-          type='password'
-          placeholder='Confirm Password'
-          name='password2'
+          type="password"
+          placeholder="Confirm Password"
+          name="password2"
           value={password2}
-          required='required'
+          required="required"
           onChange={onChange}
-          minLength='6'
+          minLength="6"
           maxLength={35}
         />
-        <input type='submit' value='Register' className={styles.btnGray} />
+        <input type="submit" value="Register" className={styles.btnGray} />
+        <Link to="/login" className={styles.signUp}>
+          Sign in
+        </Link>
       </form>
     </div>
   );

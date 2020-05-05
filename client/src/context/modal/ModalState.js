bab64a0805759d155a28f2a22960137e95ff7c3c
Nicolás Leiva Büchi <nicolasleivab@gmail.com>
2020-05-05 11:21:24 -0400

    modal context/reducer

---
+++ client/src/context/modal/ModalState.js
@@ -0,0 +1,34 @@
+import React, { useReducer } from "react";
+import ModalContext from "./modalContext";
+import modalReducer from "./modalReducer";
+import { SET_MODAL, HIDE_MODAL } from "../types";
+
+const ModalState = (props) => {
+  const initialState = {
+    modal: false,
+  };
+  const [state, dispatch] = useReducer(modalReducer, initialState);
+
+  // Set Modal
+  const setModal = () => {
+    dispatch({ type: SET_MODAL });
+  };
+  // Hide Modal
+  const hideModal = () => {
+    dispatch({ type: SET_MODAL });
+  };
+
+  return (
+    <ModalContext.Provider
+      value={{
+        modal: state.modal,
+        setModal,
+        hideModal,
+      }}
+    >
+      {props.children}
+    </ModalContext.Provider>
+  );
+};
+
+export default ModalState;
